eae0f040febd18ded5ad0a21cf88ed0717c150bc
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-29 11:52:29 +1300

    Configure most of the modules required by the docs

--- .markdown-doctest-setup.js
+++ .markdown-doctest-setup.js
@@ -1,4 +1,31 @@
+'use strict';
+let Rx = require('.');
+
+function getData () {
+  return Rx.Observable.range(1, 10).share();
+}
+
+function getOtherData () {
+  return Rx.Observable.range(1, 10).share();
+}
+
 module.exports = {
-  Rx: require('.')
+  require: {
+    'rx': Rx,
+    'fs': require('fs'),
+    'async': require('async'),
+    'events': require('events'),
+    'path': require('path'),
+    'rx-lite': require('./dist/rx.lite'),
+    'request': require('request'),
+  },
+  globals: {
+    Rx,
+    getData,
+    getOtherData,
+    transducers: require('transducers-js'),
+    setTimeout,
+    process
+  }
 }
 
